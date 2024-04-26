5f7eb1cf6024fcda3fe37360d0907ced17f7e6dd
y629 <yuutaishimoto@gmail.com>
2024-04-26 13:36:51 +0900

    add code to setup c100c

--- dataset/save_dataset.py
+++ dataset/save_dataset.py
@@ -25,6 +25,10 @@ def arr2img(arr):
 if ds == "c10":
     cifar10 = load_dataset("cifar10")
     cifar10.save_to_disk("c10") # use load_from_disk when loading for the consistency
+# cifar100
+elif ds == "c100":
+    cifar100 = load_dataset("cifar100")
+    cifar100.save_to_disk("c100") # use load_from_disk when loading for the consistency
 # cifar10-c
 elif ds == "c10c":
     # original c10
@@ -54,5 +58,34 @@ elif ds == "c10c":
         ds_dict[key] = Dataset.from_dict({"img": arr2img(ds_arr), "label": labels}, info=info)
     print(ds_dict)
     ds_dict.save_to_disk("c10c")
+# cifar100-c
+elif ds == "c100c":
+    # original c100
+    cifar100 = load_from_disk("c100")
+    # set the same info as the original
+    info = DatasetInfo(
+        features=datasets.Features(
+            {
+                "img": cifar100["train"].features["img"],
+                "label": cifar100["train"].features["fine_label"],
+            }
+        ),
+    )
+    # c100c raw data path
+    c100c_raw_dir = os.path.join("c100c/raw_data")
+    # get .npy files
+    npy_files = [f for f in os.listdir(c100c_raw_dir) if f.endswith(".npy")]
+    # make the dict (key=corruption name, val=corresponding image)
+    labels = np.load(os.path.join(c100c_raw_dir, "labels.npy"))
+    ds_dict = DatasetDict({})
+    for npy_file in npy_files:
+        key = npy_file.split(".npy")[0]
+        if key == "labels":
+            continue
+        print(f"key: {key}")
+        ds_arr = np.load(os.path.join(c100c_raw_dir, npy_file))
+        ds_dict[key] = Dataset.from_dict({"img": arr2img(ds_arr), "label": labels}, info=info)
+    print(ds_dict)
+    ds_dict.save_to_disk("c100c")
 else:
     raise NotImplementedError
