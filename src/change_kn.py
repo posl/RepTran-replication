d06f00bb325b1bb1b2d1fc08894b0b55f81e7433
y629 <yuutaishimoto@gmail.com>
2024-05-01 20:31:42 +0900

    update change_kn.py

--- src/change_kn.py
+++ src/change_kn.py
@@ -7,17 +7,19 @@ import argparse
 from datasets import load_from_disk
 from transformers import ViTForImageClassification
 from utils.helper import get_device
-from utils.vit_util import transforms
+from utils.vit_util import transforms, transforms_c100
 from utils.constant import ViTExperiment
 
 if __name__ == "__main__":
     # プログラム引数の受け取り
     parser = argparse.ArgumentParser(description='start_layer_idx selector')
+    parser.add_argument('ds_name', type=str)
     parser.add_argument('tgt_labels', type=int, nargs='+')
     parser.add_argument('--used_column', type=str, default="train")
     parser.add_argument('--start_layer_idx', type=int, default=9)
     parser.add_argument('--tgt_method', type=str, default="ig_list")
     args = parser.parse_args()
+    ds_name = args.ds_name
     tgt_labels = args.tgt_labels
     start_layer_idx = args.start_layer_idx
     used_column = args.used_column
@@ -28,18 +30,28 @@ if __name__ == "__main__":
     # デバイス (cuda, or cpu) の取得
     device = get_device()
     # datasetをロード (初回の読み込みだけやや時間かかる)
-    cifar10 = load_from_disk(os.path.join(ViTExperiment.DATASET_DIR, "c10"))
+    ds = load_from_disk(os.path.join(ViTExperiment.DATASET_DIR, ds_name))
+    
+    # datasetごとに違う変数のセット
+    if ds_name == "c10" or ds_name == "c10c":
+        tf_func = transforms
+        label_col = "label"
+    elif ds_name == "c100" or ds_name == "c100c":
+        tf_func = transforms_c100
+        label_col = "fine_label"
+    else:
+        NotImplementedError
     
     for tgt_label in tgt_labels:
         # argparseで受け取った引数のサマリーを表示
         print(f"tgt_label: {tgt_label}, start_layer_idx: {start_layer_idx}, used_column: {used_column}")
 
         # 指定したラベルだけ集めたデータセット
-        tgt_dataset = cifar10[used_column].filter(lambda x: x["label"] == tgt_label)
+        tgt_dataset = ds[used_column].filter(lambda x: x[label_col] == tgt_label)
         # 読み込まれた時にリアルタイムで前処理を適用するようにする
-        cifar10_preprocessed = tgt_dataset.with_transform(transforms)
+        ds_preprocessed = tgt_dataset.with_transform(tf_func)
         # ラベルを示す文字列のlist
-        labels = cifar10_preprocessed.features["label"].names
+        labels = ds_preprocessed.features[label_col].names
         # pretrained modelのロード
         pretrained_dir = getattr(ViTExperiment, ds_name).OUTPUT_DIR
         model = ViTForImageClassification.from_pretrained(pretrained_dir).to(device)
@@ -55,8 +67,8 @@ if __name__ == "__main__":
             print(f"op: {op}")
             all_proba = []
             # loop for the dataset
-            for data_idx, entry_dic in tqdm(enumerate(cifar10_preprocessed.iter(batch_size=batch_size)), 
-                                    total=math.ceil(len(cifar10_preprocessed)/batch_size)):
+            for data_idx, entry_dic in tqdm(enumerate(ds_preprocessed.iter(batch_size=batch_size)), 
+                                    total=math.ceil(len(ds_preprocessed)/batch_size)):
                 x, y = entry_dic["pixel_values"].to(device), entry_dic["labels"][0]
                 # バッチに対応するhidden statesとintermediate statesの取得
                 outputs = model(x, tgt_pos=tgt_pos, tgt_layer=start_layer_idx, imp_pos=kn_dict["kn"], imp_op=op)
