69486252c7106ca6056a59b3377ede790c126ec5
y629 <yuutaishimoto@gmail.com>
2024-05-14 17:09:14 +0900

    add fine-tune w/corruption datasets (#15)

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,3 +1,4 @@
+import os
 import torch
 
 
@@ -18,3 +19,12 @@ def get_device():
         device = torch.device("cpu")
     print(f"Device: {device}")
     return device
+
+def get_corruption_types():
+    # ds raw data path
+    ds_raw_dir = "/src/dataset/c10c/raw_data"
+    # get .npy files
+    npy_files = [f for f in os.listdir(ds_raw_dir) if f.endswith(".npy")]
+    # npy_filesのファイル名だけを保存
+    corruption_types = [f.split(".npy")[0] for f in npy_files if f != "labels.npy"]
+    return corruption_types
